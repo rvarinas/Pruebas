3e4e9b383d37de3760a2366df9a1366d252a72fb
Ricardo Varinas <ricardoescom@gmail.com>
2023-01-12 13:57:29 -0600

    Actualización de practica

--- PracticaCalculadora.py
+++ PracticaCalculadora.py
@@ -8,7 +8,10 @@ def menu():
         print('2.- Resta')
         print('3.- Multiplicación')
         print('4.- División')
-        print('5.- Salir')
+        print('5.- Raiz n')
+        print('6.- Exponente n')
+        print('7.- Seno')
+        print('8.- Salir')
 
 while True:
 
@@ -18,7 +21,15 @@ while True:
     
     if opciones_menu=='1':
         print('')
-        input('Seleccionaste la suma')
+        print('Seleccionaste la suma')
+        num1=int(input('Ingresa el primer valor: '))
+        num2=int(input('Ingresa el segundo valor: '))
+        res=0
+        res=num1+num2
+        print('El resultado es: ',res)
+        print('')
+        input('Enter para regresar al menu')
+        
     elif opciones_menu=='2':
         print('')
         input('Seleccionaste la resta')
@@ -29,6 +40,11 @@ while True:
         print('')
         input('Seleccionaste la división')
     elif opciones_menu=='5':
+        print('')
+        print('Seleccionaste la raiz')
+        input('Ingresa el número base: ')
+
+    elif opciones_menu=='8':
         break
     else:
         print('')
